6e293ca5f8a610500df2b9e6ead19069bdde6c15
Pavlos Melissinos <pavlos@baresquare.com>
2022-09-26 21:07:54 +0300

    Add a couple REPL lifecycle functions

---
+++ dev/dev.clj
@@ -0,0 +1,37 @@
+(ns dev
+  (:require [cljfx.api :as fx]
+            [clodenames2.ui :as ui]
+            [clodenames2.core :as core]))
+
+(defonce renderer (atom nil)) ;; maybe this isn't necessary, we'll see
+
+(defn go
+  ([]
+   (go (core/load-config)))
+  ([m]
+   (try
+     (fx/unmount-renderer ui/*state @renderer)
+     (catch Throwable ex
+       nil))
+   (reset! renderer ui/renderer)
+   (reset! ui/*state (core/init m))
+   (fx/mount-renderer ui/*state @renderer)
+   @ui/*state))
+
+(defn halt! []
+  (if @renderer
+    (fx/unmount-renderer ui/*state @renderer))
+  (reset! ui/*state nil)
+  (reset! renderer nil))
+
+(defn reset [{:keys [cfg] :as m}]
+  (println "Halting old state")
+  (halt!)
+  (println "Starting new state")
+  (go cfg))
+
+(comment
+  (def sys (go))
+  (reset sys)
+  (halt!)
+  )
