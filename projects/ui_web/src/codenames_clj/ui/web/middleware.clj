c3e4e377feda3d31b55bd67bfe12ae3ab56c6b24
Pavlos <PavlosMelissinos@users.noreply.github.com>
2026-05-25 02:58:21 +0300

    chore: Upgrade biff to v0.7.15 (#51)

--- projects/ui_web/src/codenames_clj/ui/web/middleware.clj
+++ projects/ui_web/src/codenames_clj/ui/web/middleware.clj
@@ -1,5 +1,8 @@
 (ns codenames-clj.ui.web.middleware
   (:require [com.biffweb :as biff]
+            [muuntaja.middleware :as muuntaja]
+            [ring.middleware.anti-forgery :as csrf]
+            [ring.middleware.defaults :as rd]
             [xtdb.api :as xt]))
 
 (defn wrap-redirect-signed-in [handler]
@@ -27,3 +30,44 @@
         (handler (assoc ctx :player player :match match))
         {:status 303
          :headers {"location" "/"}}))))
+
+;; Stick this function somewhere in your middleware stack below if you want to
+;; inspect what things look like before/after certain middleware fns run.
+(defn wrap-debug [handler]
+  (fn [ctx]
+    (let [response (handler ctx)]
+      (println "REQUEST")
+      (biff/pprint ctx)
+      (def ctx* ctx)
+      (println "RESPONSE")
+      (biff/pprint response)
+      (def response* response)
+      response)))
+
+(defn wrap-site-defaults [handler]
+  (-> handler
+      biff/wrap-render-rum
+      biff/wrap-anti-forgery-websockets
+      csrf/wrap-anti-forgery
+      biff/wrap-session
+      muuntaja/wrap-params
+      muuntaja/wrap-format
+      (rd/wrap-defaults (-> rd/site-defaults
+                            (assoc-in [:security :anti-forgery] false)
+                            (assoc-in [:responses :absolute-redirects] true)
+                            (assoc :session false)
+                            (assoc :static false)))))
+
+(defn wrap-api-defaults [handler]
+  (-> handler
+      muuntaja/wrap-params
+      muuntaja/wrap-format
+      (rd/wrap-defaults rd/api-defaults)))
+
+(defn wrap-base-defaults [handler]
+  (-> handler
+      biff/wrap-https-scheme
+      biff/wrap-resource
+      biff/wrap-internal-error
+      biff/wrap-ssl
+      biff/wrap-log-requests))
